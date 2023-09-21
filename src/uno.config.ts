981b7bb1adce10e0919d0d21bc998cadff054651
Fatpandac <i@fatpandac.com>
2023-09-21 15:24:57 +0800

    feat: change to use unocss-preset-useful

--- src/uno.config.ts
+++ src/uno.config.ts
@@ -1,48 +1,9 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-// eslint-disable-next-line no-restricted-imports
-import { defineConfig, presetAttributify, presetIcons, presetUno, presetWebFonts } from "unocss";
-
-export const defaultConfig = defineConfig<object>({
-  details: true,
-  presets: [
-    presetUno(),
-    presetAttributify(),
-    presetIcons({
-      scale: 1.2,
-      cdn: "https://esm.sh/",
-    }),
-  ],
-});
+import { defineConfig } from 'unocss'
+import { presetUseful } from 'unocss-preset-useful'
 
 export default defineConfig({
-  ...defaultConfig,
+  details: true,
   presets: [
-    ...defaultConfig.presets!,
-    presetIcons({
-      scale: 1.2,
-    }),
-    presetWebFonts({
-      fonts: {
-        sans: "Inter:100,200,400,700,800",
-        mono: "IBM Plex Mono",
-      },
-    }),
+    presetUseful(),
   ],
-  shortcuts: [
-    [/^badge-(.*)$/, ([, c]) => `bg-${c}4:10 text-${c}5 rounded`],
-    [/^badge-xs-(.*)$/, ([, c]) => `badge-${c} text-xs px2 py0.5`],
-    [/^badge-sm-(.*)$/, ([, c]) => `badge-${c} text-sm px3 py0.6`],
-    [/^badge-lg-(.*)$/, ([, c]) => `badge-${c} px3 py0.8`],
-    [
-      /^badge-square-(.*)$/,
-      ([, c]) => `badge-${c} w-7 h-7 text-lg font-200 flex flex-none items-center justify-center`,
-    ],
-    {
-      "border-base": "border-gray-500:10",
-      "bg-base": "bg-white dark:bg-hex-121212",
-      btn: "px-4 py-1 inline-block bg-cyan6 hover:bg-cyan7 text-white cursor-pointer disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50",
-      link: "op50 hover:op100 hover:text-cyan6",
-      divider: "border-b border-base",
-    },
-  ],
-});
+})
--- src/utils/search.ts
+++ src/utils/search.ts
@@ -2,5 +2,6 @@ import { createGenerator } from "unocss";
 import { createSearch } from "../share-docs";
 import  defaultConfig  from "../uno.config";
 
-const uno = createGenerator(defaultConfig);
+const uno = createGenerator({}, defaultConfig);
+
 export const searcher = createSearch({ uno });
